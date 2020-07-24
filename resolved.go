fc0d3b2d53e3788e0fc1ba1e91f05f12afe945d5
Josef Filzmaier <j.filzmaier@gmx.at>
2020-07-24 12:51:33 +0200

    Add some comments

--- resolved.go
+++ resolved.go
@@ -12,6 +12,7 @@ var (
 	likPath     = "org.freedesktop.resolve1.Link"
 )
 
+// New instantiates a new Resolved object allowing to manage systemd-resolved
 func New() (*Resolved, error) {
 	conn, err := dbus.SystemBus()
 	if err != nil {
@@ -23,16 +24,19 @@ func New() (*Resolved, error) {
 	}, nil
 }
 
+// Resolved Central type for invoking methods or reading properties from systemd-resolved
 type Resolved struct {
 	conn   *dbus.Conn
 	sysObj dbus.BusObject
 	io.Closer
 }
 
+// Close closes the underlying dbus connection
 func (r *Resolved) Close() error {
 	return r.conn.Close()
 }
 
+// Introspect list available methods
 func (r *Resolved) Introspect() (*introspect.Node, error) {
 	node, err := introspect.Call(r.conn.Object("org.freedesktop.resolve1", "/org/freedesktop/resolve1"))
 	if err != nil {
@@ -41,6 +45,7 @@ func (r *Resolved) Introspect() (*introspect.Node, error) {
 	return node, nil
 }
 
+// GetLink takes a network interface index and returns a Link object allowing to manipulate it
 func (r *Resolved) GetLink(idx int32) (l *Link, err error) {
 	var obj dbus.ObjectPath
 	if err = r.sysObj.Call(managerPath+".GetLink", 0, idx).Store(&obj); err != nil {
@@ -52,35 +57,36 @@ func (r *Resolved) GetLink(idx int32) (l *Link, err error) {
 	return
 }
 
+// SetLinkDNS set dns entries for a given link
 func (r *Resolved) SetLinkDNS(link int32, dnsEntries []*DnsEntry) error {
 	c := r.sysObj.Call(managerPath+".SetLinkDNS", 0, link, dnsEntries)
 	return c.Err
 }
 
 func (r *Resolved) ResetStatistics() {
-
+	//TODO
 }
 
 func (r *Resolved) SetLinkDomains() {
-
+	//TODO
 }
 
 func (r *Resolved) SetLinkLLMNR() {
-
+	//TODO
 }
 
 func (r *Resolved) SetLinkMulticastDNS() {
-
+	//TODO
 }
 
 func (r *Resolved) SetLinkDNSSEC() {
-
+	//TODO
 }
 
 func (r *Resolved) SetLinkDNSSECNegativeTrustAnchors() {
-
+	//TODO
 }
 
 func (r *Resolved) RevertLink() {
-
+	//TODO
 }
